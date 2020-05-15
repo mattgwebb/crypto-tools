5b0f8c7863443600deaf818f21b8edcabb30afb9
mattynwa <mattwebb.ynwa@gmail.com>
2020-05-15 17:50:42 +0200

    Strategy table wasn´t added to migrations

--- src/Migrations/Version20200425142630.php
+++ src/Migrations/Version20200425142630.php
@@ -22,6 +22,8 @@ final class Version20200425142630 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE strategy (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
         $this->addSql('CREATE TABLE algo_strategies (id INT AUTO_INCREMENT NOT NULL, algo_id INT DEFAULT NULL, strategy_id INT DEFAULT NULL, type INT NOT NULL, INDEX IDX_CAABCF101ECC0724 (algo_id), INDEX IDX_CAABCF10D5CAD932 (strategy_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE algo_strategies ADD CONSTRAINT FK_CAABCF101ECC0724 FOREIGN KEY (algo_id) REFERENCES bot_algorithm (id)');
         $this->addSql('ALTER TABLE algo_strategies ADD CONSTRAINT FK_CAABCF10D5CAD932 FOREIGN KEY (strategy_id) REFERENCES strategy (id)');
@@ -33,5 +35,6 @@ final class Version20200425142630 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE algo_strategies');
+        $this->addSql('DROP TABLE strategy');
     }
 }
